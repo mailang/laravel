cbcbb240a35c9d5b73f7c621a6d04c4306549255
cpcode <1065096458@qq.com>
2017-10-25 14:47:08 +0800

    summaryform
    汇总表结构完善

--- database/migrations/2017_10_24_084136_create_summaryform_table.php
+++ database/migrations/2017_10_24_084136_create_summaryform_table.php
@@ -15,8 +15,26 @@ class CreateSummaryformTable extends Migration
     {
         Schema::create('summaryform', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
+    
+             //users表中id
+            $table->increments('uid');
+             //地区
+            $table->string('areacode');
+             //年初
+            $table->string('beginer_year');
+             //发生数
+            $table->string('occur_number');
+             //年末
+            $table->string('end_year');
+             //说明
+             $table->string('description');
+             
+                     $table->timestamps();
         });
+
+      Schema::table('summaryform', function($table) {
+        $table->foreign('uid')->references('id')->on('users');
+      });
     }
 
     /**
