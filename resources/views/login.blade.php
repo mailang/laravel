e020ac67a6660c98d1ae9549acca40a33ebf9435
hunterlr <hongshouzh@163.com>
2018-04-04 12:13:02 +0800

    浏览器下载

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -78,9 +78,9 @@
         {{--<a href="register.html" class="text-center">Register a new membership</a>--}}
 
       </div><!-- /.login-box-body -->
-     <div style="padding-top: 100px; color:white; font-size: 12px; text-align: center;">   联系方式：18949895245 </div>
+      <div style="text-align: center;"><a href="/download/360cse.exe" style=" color:white; font-size: 12px; text-align: center;">浏览器下载</a></div>
+      <div style="padding-top: 100px; color:white; font-size: 12px; text-align: center;">   联系方式：18949895245 </div>
       <div style=" color:white; font-size: 12px; text-align: center;">   操作问题 请加 QQ 群：255459368 </div>
-      <div style=" color:white; font-size: 12px; text-align: center;"><a href="/download/360cse.exe">浏览器下载</a></div>
     </div><!-- /.login-box -->
 
     <!-- jQuery 2.1.4 -->
